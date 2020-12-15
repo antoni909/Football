9a0f5b1de6dcc77a4924b019f9431421b9d162d9
antoni <lorenzo.antonio.090@gmail.com>
2020-12-14 23:34:10 -0800

    created functions and worked on refactoring js code completely

--- int.js
+++ int.js
@@ -1,16 +1,49 @@
-var response = prompt("Are you a beaver or duck fanatic?");
+var teamChoice = ""; //globally declaring this var
 
-var userInput;
-
-if (response === "beaver") {
-    userInput = alert( "congratulations you are on the winning team! :D ");
-} else if (response === "duck") {
-    userInput = alert("FAIL!!! you are on the losing team!!!");
-} else  {
-    userInput === alert("You're a platypus!!!XD");
+function usrReply() {
+    var prmptQuestion = "Are you a beaver or duck Fan?!!!"
+    var userInput = prompt(prmptQuestion);
+    console.log(userInput);
+    myMessage(userInput);
 }
-console.log(response);
-console.log(userInput);
 
-document.getElementById("user-input").innerHTML = response;
+usrReply();
+
+function myMessage(userInput) {
+    teamChoice = userInput;
+    console.log(userInput);
+    if (userInput === "beaver") {
+        alert( "congratulations you are on the winning team! :D ");       
+    } else if (userInput === "duck") {
+        alert("FAIL!!! you are on the losing team!!!");
+    } else  {
+        alert("You're a platypus!!!XD");
+    }
+}    
+
+document.getElementById("user-input").innerHTML = teamChoice;
+
+// OLD code below
+
+//myMessage("");
+
+//var response = prompt("Are you a beaver or duck fanatic?");
+
+//var userInput;   
+//console.log(userInput); //undefined because it has only been declared
+
+
+// if (response === "beaver") {
+//     userInput = alert( "congratulations you are on the winning team! :D ");
+//     console.log(userInput); //why is userInput undefined?
+// } else if (response === "duck") {
+//     userInput === alert("FAIL!!! you are on the losing team!!!");
+// } else  {
+//     userInput === alert("You're a platypus!!!XD");
+// }
+
+//console.log(response);  //response value is reassigned per users input..beaver, duck, else
+//console.log(userInput); //why is this undefined?
+
+//document.getElementById("user-input").innerHTML = teamChoice;
 
