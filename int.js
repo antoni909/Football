c51f68a0c03974c318a993e590661fd381689eca
antoni <lorenzo.antonio.090@gmail.com>
2020-12-16 13:34:00 -0800

    i managed to fix the addStarToDocument function and using the returned value to pass to the for loop that displays the correct number of stars

--- int.js
+++ int.js
@@ -24,6 +24,7 @@
 
 // var teamChoice = ""; //globally declaring this var
 
+//********Refactored Code Written Below***********
 
 // Here I will create a Password Prompt
 
@@ -33,7 +34,6 @@ var secretPassword = "Football";    //the expected password stored in its variab
 
 do {                        
     var userPasswordEntry = prompt("What is the Password?");   //the users input is stored in the variable (userPasswordEntry) via prompt()
-
 } while (userPasswordEntry !== secretPassword);
 
 // Below is my refactored code that stores user input and slaps it on the HTML page
@@ -42,21 +42,21 @@ function usrReply() {
     var prmptQuestion = "Are you a beaver or duck Fan?!!!"
     var userInput = prompt(prmptQuestion);
     myMessage(userInput);
-}
+ }
 
 function myMessage(userInput) {
     teamChoice = userInput;
     console.log(userInput);
-    if (userInput === "beaver") {
-        alert( "congratulations you the better team! :D ");
-        color = "orange"       
-    } else if (userInput === "duck") {
-        alert("FAIL!!! you are on the losing team!!!");
-        color = "yellow"
-    } else  {
-        alert("You're a platypus!!!XD");
-        color = "grey"
-    }
+        if (userInput === "beaver") {
+            alert( "congratulations you the better team! :D ");
+            color = "orange"       
+        } else if (userInput === "duck") {
+            alert("FAIL!!! you are on the losing team!!!");
+            color = "yellow"
+        } else  {
+            alert("You're a platypus!!!XD");
+            color = "grey"
+        }
 }    
 
 var color; //declare the background color
@@ -67,25 +67,20 @@ usrReply();
 document.getElementById("user-input").innerHTML = teamChoice;
 document.getElementById("divColor").style.backgroundColor = color;
 
-
-
-
-
 //Here I will create a prompt for the user to ask how many stars would they rate the USABILITY and LOGIC of the page
 
-function addStarToDocument(imageName) {
-    var numberOfStars = prompt("From 5 stars being Great and 1 star being Poor, How many Stars would you like to give this page?"); {
-            //statement
-    }
+function addStarToDocument() {
+    var userResponse = prompt("From 5 stars being Great and 1 star being Poor, How many Stars would you like to give this page?"); 
+    return userResponse;
+}
+// originally in the funciton above, i had not declared userResponse and in turn it would not return the value of userResponse. I fixed it by declareing the userResponse as written in the function above.
+
+var usrRating = addStarToDocument();
+
+//Here I will create a For loop that will generate the desired number of "stars" based on the value passed from the addStarToDocument function in the usrRating variable
+
+for (var i = 1; i <= usrRating; i++) {
     document.write("<img src = \"images/star.jpg\">");
 }
 
-addStarToDocument();
-
-//Here I will create a For loop that will produce a Number of stars (an image src) based on the desired rating
-
-for (var i = 0; i < numberOfStars; i++); {
-    console.log(i);
-    addStarToDocument();
-}
-
+//it works. Next step is to refactor my code and make it more efficient by perhaps reusing functions or re-purposing user data
